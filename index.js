0367649d1fbadac5be8486e0e241cb297afd40a9
guilherme lopes <guilherme.ghlopes@gmail.com>
2023-05-27 12:39:26 -0300

    revisões planilhas fornecedores

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function atualizarClientes() {
-    const clientes = fetch('http://localhost:3000/cliente')
+    const ul = document.getElementById('listaContatos')
+    ul.innerHTML = ''
+    
+    const clientes = fetch('https://exemplo-api-teste-seft.onrender.com/cliente')
 .then(resposta => resposta.json())
 .then(clientesDados => {
     clientesDados.forEach(cliente => {
         const li = document.createElement('li')
         li.innerText = `${cliente.nome} - ${cliente.email}`
+
+// Botão deletar
+const btnDelete = document.createElement('button')
+btnDelete.innerText = "Remover"
+btnDelete.className = "btn btn-danger"
+btnDelete.addEventListener('click',() => deletarCliente(cliente.id))
+li.appendChild(btnDelete)
+
+
         const ul = document.getElementById('listaClientes')
         ul.appendChild(li)
     })
@@ -15,15 +27,74 @@ function atualizarClientes() {
 }
 atualizarClientes()
 
+const formCadastro = document.getElementById('formCliente')
+formCadastro.addEventListener('submit',(event) => {
+    event.preventDefault()
+    cadastrarCliente(event)
+})
+
+function cadastrarCliente(form){
+    const clienteNovo = {
+        nome: form.target.nome.value,
+        email: form.target.email.value,
+        telefone: form.target.telefone.value
+    }
+    fetch('https://exemplo-api-teste-seft.onrender.com/cliente',{
+        method: 'POST',
+        headers: {'Content-type':'application/json'},
+        body: JSON.stringify(clienteNovo)
+
+    })
+    .then(resposta => {
+        if(resposta.status != 201){
+            alert('Erro ao cadastrar!')
+            return
+        }
+
+           
+        alert('Cadastro com sucesso!')
+        atualizarCliente()
+    })
+}
+
+function deletarCliente(id){
+    fetch(`https://exemplo-api-teste-seft.onrender.com/cliente'/${id}`,{
+        method: 'DELETE'
+    })
+    .then(resposta => {
+        if(resposta.status != 200){
+            alert('Erro ao excluir')
+        }
+        atualizarClientes()
+    })
+
+}
+
+
+
+
+
 function atualizarFornecedores() {
-const fornecedores = fetch('http://localhost:3000/fornecedor')
+    const u2 = document.getElementById('listaContatos')
+    u2.innerHTML = ''
+
+const fornecedores = fetch('https://exemplo-api-teste-seft.onrender.com/fornecedor')
 .then(resposta => resposta.json())
 .then(fornecedoresDados => {
     fornecedoresDados.forEach(fornecedor => {
-        const li = document.createElement('li')
-        li.innerText = `${fornecedor.nome} - ${fornecedor.email}`
-        const ul = document.getElementById('listaFornecedores')
-        ul.appendChild(li)
+        const li2 = document.createElement('li2')
+        li2.innerText = `${fornecedor.nome} - ${fornecedor.email}`
+
+        //botão deletar
+        const btnDelete = document.createElement('button')
+btnDelete.innerText = "Remover"
+btnDelete.className = "btn btn-danger"
+btnDelete.addEventListener('click',() => deletarFornecedor(fornecedor.id))
+li.appendChild(btnDelete)
+
+
+        const u2 = document.getElementById('listaFornecedores')
+        u2.appendChild(li2)
     })
 
     
@@ -33,16 +104,72 @@ const fornecedores = fetch('http://localhost:3000/fornecedor')
 
 atualizarFornecedores()
 
+const formCadastro2 = document.getElementById('formFornecedor')
+formCadastro2.addEventListener('submit',(event) => {
+    event.preventDefault()
+    cadastrarFornecedor(event)
+})
+
+function cadastrarFornecedor(form2){
+    const fornecedorNovo = {
+        nome: form2.target.nome.value,
+        email: form2.target.email.value,
+        telefone: form2.target.telefone.value
+    }
+    fetch('https://exemplo-api-teste-seft.onrender.com/fornecedor',{
+        method: 'POST',
+        headers: {'Content-type':'application/json'},
+        body: JSON.stringify(fornecedorNovo)
+
+    })
+    .then(resposta => {
+        if(resposta.status != 201){
+            alert('Erro ao cadastrar!')
+            return
+        }
+
+          
+        alert('Cadastro com sucesso!')
+        atualizarFornecedor()
+    })
+}
+
+function deletarFornecedor(id){
+    fetch(`https://exemplo-api-teste-seft.onrender.com/fornecedor'/${id}`,{
+        method: 'DELETE'
+    })
+    .then(resposta => {
+        if(resposta.status != 200){
+            alert('Erro ao excluir')
+        }
+        atualizarFornecedores()
+    })
+
+}
+
+
 
 function atualizarProdutos() {
-const produtos = fetch('http://localhost:3000/produto')
+    const u3 = document.getElementById('listaContatos')
+    u3.innerHTML = ''
+
+
+const produtos = fetch('https://exemplo-api-teste-seft.onrender.com/produto')
 .then(resposta => resposta.json())
 .then(produtosDados => {
     produtosDados.forEach(produto => {
-        const li = document.createElement('li')
-        li.innerText = `${produto.nome} - ${produto.email}`
-        const ul = document.getElementById('listaProdutos')
-        ul.appendChild(li)
+        const li3 = document.createElement('li3')
+        li3.innerText = `${produto.nome} - ${produto.email}`
+
+        const btnDelete = document.createElement('button')
+        btnDelete.innerText = "Remover"
+        btnDelete.className = "btn btn-danger"
+        btnDelete.addEventListener('click',() => deletarProduto(produto.id))
+        li.appendChild(btnDelete)
+
+
+        const u3 = document.getElementById('listaProdutos')
+        u3.appendChild(li3)
     })
 
     
@@ -51,3 +178,46 @@ const produtos = fetch('http://localhost:3000/produto')
 }
 
 atualizarProdutos()
+
+const formCadastro3 = document.getElementById('formProduto')
+formCadastro3.addEventListener('submit',(event) => {
+    event.preventDefault()
+    cadastrarProduto(event)
+})
+
+function cadastrarProduto(form3){
+    const produtoNovo = {
+        nome: form3.target.nome.value,
+        email: form3.target.email.value,
+        telefone: form3.target.telefone.value
+    }
+    fetch('https://exemplo-api-teste-seft.onrender.com/produto',{
+        method: 'POST',
+        headers: {'Content-type':'application/json'},
+        body: JSON.stringify(produtoNovo)
+
+    })
+    .then(resposta => {
+        if(resposta.status != 201){
+            alert('Erro ao cadastrar!')
+            return
+        }
+
+        
+        alert('Cadastro com sucesso!')
+        atualizarProdutos()
+    })
+}
+
+function deletarCliente(id){
+    fetch(`https://exemplo-api-teste-seft.onrender.com/cliente/produto'/${id}`,{
+        method: 'DELETE'
+    })
+    .then(resposta => {
+        if(resposta.status != 200){
+            alert('Erro ao excluir')
+        }
+        atualizarClientes()
+    })
+
+}
